dc9afabd6ae5d13ba97a424a763a0886b5dd90b8
DESKTOP-PL1SE9F\dhffk <choibh6258@hanyang.ac.kr>
2022-07-12 20:31:34 +0900

    [기능추가] Header 컴포넌트
    Header 컴포넌트 추가와 폴더 정리

--- src/App.js
+++ src/App.js
@@ -1,9 +1,16 @@
-import './App.css';
+import './assets/css/App.css';
+import React from "react";
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import MainPage from './pages/Main';
 
 function App() {
   return (
     <div className="App">
-      
+      <BrowserRouter>
+        <Routes>
+          <Route path="/main" element={<MainPage/>}/>
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
---
+++ src/components/Header.js
@@ -0,0 +1,45 @@
+import React from 'react';
+import NotificationsNoneIcon from '@mui/icons-material/NotificationsNone';
+import AssignmentOutlinedIcon from '@mui/icons-material/AssignmentOutlined';
+import PersonOutlineOutlinedIcon from '@mui/icons-material/PersonOutlineOutlined';
+import IconButton from "@mui/material/IconButton";
+import SearchIcon from "@mui/icons-material/Search";
+import TextField from "@mui/material/TextField";
+
+function Header(){
+    return(
+        <div className='Header'>
+            <nav>
+                <a className="nav_logo" style={{fontWeight:"bold",fontSize:"36px"}}>
+                    DeVin
+                </a>
+                <ul style={{listStyleType:"None"}}>
+                    <li>프로젝트</li>
+                    <li>스터디</li>
+                    <li>Q&A</li>
+                </ul>
+                <div className="icon_wrapper">
+                    <form>
+                        <TextField
+                        id="search-bar"
+                        className="text"
+                        label=""
+                        variant="outlined"
+                        placeholder="Search..."
+                        size="small"
+                        style={{width:"170px"}}
+                        />
+                        <IconButton type="submit" aria-label="search">
+                        <SearchIcon style={{ fill: "gray" }} />
+                        </IconButton>
+                    </form>
+                    <NotificationsNoneIcon className='nav_icon'/>
+                    <AssignmentOutlinedIcon className='nav_icon'/>
+                    <PersonOutlineOutlinedIcon className='nav_icon'/>
+                </div>
+            </nav>
+        </div>
+    );
+}
+
+export default Header
